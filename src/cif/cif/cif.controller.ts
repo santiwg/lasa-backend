2798f151da07e8de698b60eaaf9b585e1daf75ce
santiwg <santiwgwuri@gmail.com>
2025-08-15 19:40:30 -0300

    Agrego endpoint de cifs que trae toda la data necesaria para el front junta

--- src/cif/cif/cif.controller.ts
+++ src/cif/cif/cif.controller.ts
@@ -8,22 +8,36 @@ import { PaginationDto } from 'src/shared/pagination/dtos/pagination.dto';
 @Controller('cifs')
 export class CifController {
     constructor(private readonly cifService: CifService) { }
-    
+
     @Get()
     async findAll(@Query() pagination: PaginationDto): Promise<PaginatedResponseDto<Cif>> {
         return await this.cifService.findAllPaginated(pagination);
     }
-    
+
     @Get('current-month-total')
     async findCurrentMonthTotal(): Promise<number> {
         return await this.cifService.getCurrentMonthTotal();
     }
-    
+
     @Get('last-month-total')
     async findLastMonthTotal(): Promise<number> {
         return await this.cifService.getLastMonthTotal();
     }
-    
+
+    //The next endpoint provides all necessary for the front-end in one call
+    @Get('summary')
+    async getSummary(@Query() pagination: PaginationDto): Promise<{
+        paginatedCifs: PaginatedResponseDto<Cif>,
+        currentMonthTotal: number,
+        lastMonthTotal: number
+    }> {
+        return {
+            paginatedCifs: await this.cifService.findAllPaginated(pagination),
+            currentMonthTotal: await this.cifService.getCurrentMonthTotal(),
+            lastMonthTotal: await this.cifService.getLastMonthTotal(),
+        };
+    }
+
     @Post()
     async createCif(@Body() newCif: NewCifDto): Promise<Cif> {
         return await this.cifService.createCif(newCif);
