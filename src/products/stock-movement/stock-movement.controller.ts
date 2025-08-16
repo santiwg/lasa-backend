5ed1941b4d3e4975b33021eb84a61bc615fe822b
santiwg <santiwgwuri@gmail.com>
2025-08-16 20:50:03 -0300

    Agrego tipado a las respuestas del controlador de stock-movement

--- src/products/stock-movement/stock-movement.controller.ts
+++ src/products/stock-movement/stock-movement.controller.ts
@@ -1,17 +1,18 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { StockMovementService } from './stock-movement.service';
 import { NewStockMovementDto } from './dtos/newStockMovement.dto';
+import { StockMovement } from './stock-movement.entity';
 
 @Controller('stock-movements')
 export class StockMovementController {
     constructor(private readonly stockMovementService: StockMovementService) {}
 
     @Get()
-    findAll() {
+    findAll():Promise<StockMovement[]> {
         return this.stockMovementService.findAll();
     }
     @Post()
-    create(@Body() createStockMovementDto: NewStockMovementDto) {
+    create(@Body() createStockMovementDto: NewStockMovementDto):Promise<StockMovement> {
         return this.stockMovementService.create(createStockMovementDto);
     }
 }
