472869278f274eec6fb414f713219a506e6ec6c0
santiwg <santiwgwuri@gmail.com>
2025-08-31 21:31:11 -0300

    Agrego chequeo de tipo en auth middleware

--- src/auth/middlewares/auth.middleware.ts
+++ src/auth/middlewares/auth.middleware.ts
@@ -22,7 +22,7 @@ export class AuthGuard implements CanActivate {
   async canActivate(context: ExecutionContext): Promise<boolean> {
     try {
       const request: RequestWithUser = context.switchToHttp().getRequest();
-      const authHeader = request.headers.authorization;
+      const authHeader = request.headers?.authorization;
       if (!authHeader) {
         throw new UnauthorizedException('El token no existe');
       }
